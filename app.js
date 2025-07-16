935c57a055df1616b2bf7a174348e79fe049786a
AngcGabriel-rgb <gabrielange433@gmail.com>
2025-07-16 16:02:45 +0100

    api5

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ app.use((err, req, res, next) => {
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur le port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Serveur démarré sur le port ${PORT}`);
 });
